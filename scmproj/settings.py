bb51d7fb4a3eb7c2a097d6723f841dcbcf8452cd
Dave Petrillo <david.petrillo@gmail.com>
2013-02-01 18:09:03 -0500

    added database settings

--- scmproj/settings.py
+++ scmproj/settings.py
@@ -28,7 +28,11 @@ DATABASES = {
     }
 }
 
-#DATABASES['default'] =  dj_database_url.config()
+try:
+    if DATABASE_URL:
+        DATABASES['default'] =  dj_database_url.config()
+except:
+    pass
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
