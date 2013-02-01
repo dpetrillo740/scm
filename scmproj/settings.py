c9132ea106b3f73e2f4133b30ea80413bc8c008b
Dave Petrillo <david.petrillo@gmail.com>
2013-02-01 18:36:25 -0500

    More changes to db settings

--- scmproj/settings.py
+++ scmproj/settings.py
@@ -15,10 +15,24 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+DATABASES = {
+    'default': {
+        #'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+        #'NAME': os.path.join(SITE_ROOT, 'db-development.sqlite3'), # Or path to database file if using sqlite3.
+        'NAME': 'dev_database_scm',
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
 
-DATABASES['default'] =  dj_database_url.config()
-
-    
+try:
+    if DATABASE_URL:
+        DATABASES['default'] =  dj_database_url.config(default='postgres://localhost')
+except:
+    pass
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
