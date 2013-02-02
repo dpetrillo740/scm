2a5540abe80291be2b8c808eab4be5171b8c1bfd
Dave Petrillo <david.petrillo@gmail.com>
2013-02-01 19:04:09 -0500

    gb

--- scmproj/settings.py
+++ scmproj/settings.py
@@ -21,9 +21,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', 
         #'NAME': os.path.join(SITE_ROOT, 'db-development.sqlite3'), # Or path to database file if using sqlite3.
         'NAME': 'dev_database_scm',
-        'USER': 'Dpetrillo',                      # Not used with sqlite3.
+        'USER': 'Dpetrillo ',                      # Not used with sqlite3.
         'PASSWORD': 'postgres',                  # Not used with sqlite3.
-        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
